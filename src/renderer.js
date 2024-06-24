b027a9e52351934db1101bb75e706e0d2eb7253b
MiMillieuh <52078885+MiMillieuh@users.noreply.github.com>
2024-06-24 10:22:36 +0200

    Fix reset OC due to rewrite

--- src/renderer.js
+++ src/renderer.js
@@ -1,91 +1,83 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const enablePowerManagementCheckbox = document.getElementById("enablepowermanagement");
-    const slider1 = document.getElementById('slider1');
-    const value1 = document.getElementById('value1');
-    const slider2 = document.getElementById('slider2');
-    const value2 = document.getElementById('value2');
-    const slider3 = document.getElementById('slider3');
-    const value3 = document.getElementById('value3');
+function sendReady() {
+    window.api.send("ready", true);
+}
 
-    enablePowerManagementCheckbox.addEventListener("change", (event) => {
-        if (event.target.checked) {
-            if (confirm("Are you sure you want to enable power management?\nThis is a dangerous feature and you should know what you're doing before enabling it.\nThis feature won't work on Laptop GPU.")) {
-                slider3.disabled = false;
-                value3.disabled = false;
-            } else {
-                event.target.checked = false;
-            }
-        } else {
-            slider3.disabled = true;
-            value3.disabled = true;
-        }
-    });
-
-    slider3.disabled = true;
-    value3.disabled = true;
-
-    window.api.receive("fromMain", (data) => {
-        console.log(data);
-        slider1.value = data[1];
-        value1.value = data[1];
-        slider2.value = data[2] / 2;
-        value2.value = data[2] / 2;
-
-        if (data[3]) {
-            slider3.value = data[3];
-            value3.value = data[3];
-            enablePowerManagementCheckbox.checked = true;
-            slider3.disabled = false;
-            value3.disabled = false;
-        }
-    });
+window.api.receive("fromMain", (data) => {
+    console.log(data);
+    document.getElementById('slider1').value = data[1];
+    document.getElementById('value1').value = data[1];
+    document.getElementById('slider2').value = data[2]/2;
+    document.getElementById('value2').value = data[2]/2;
+    if (data[3]) {
+        document.getElementById('slider3').value = data[3];
+        document.getElementById('value3').value = data[3];
+        document.getElementById("enablepowermanagement").checked = true;
+        document.getElementById('slider3').disabled = false;
+        document.getElementById('value3').disabled = false;
+    }
 });
 
-function sendToMain(channel, message) {
-    window.api.send(channel, message);
-}
-
-function exemple() {
-    sendToMain("toMain", "installps");
-}
-
-function sendReady() {
-    sendToMain("ready", true);
-}
-
 function updateValue(sliderNumber) {
-    const slider = document.getElementById(`slider${sliderNumber}`);
-    const value = document.getElementById(`value${sliderNumber}`);
+    let slider = document.getElementById(`slider${sliderNumber}`);
+    let value = document.getElementById(`value${sliderNumber}`);
     value.value = slider.value;
 }
 
 function updateSlider(sliderNumber) {
-    const slider = document.getElementById(`slider${sliderNumber}`);
-    const value = document.getElementById(`value${sliderNumber}`);
-    const val = parseFloat(value.value);
-    if (val >= slider.min && val <= slider.max) {
+    let slider = document.getElementById(`slider${sliderNumber}`);
+    let value = document.getElementById(`value${sliderNumber}`);
+    if (value.value >= slider.min && value.value <= slider.max) {
         slider.value = value.value;
+    } else {
+
     }
 }
 
 function getSliderValues() {
-    const slider1Value = document.getElementById('value1').value;
-    const slider2Value = document.getElementById('value2').value;
+    let slider1Value = document.getElementById('value1').value;
+    let slider2Value = document.getElementById('value2').value;
     let slider3Value = document.getElementById('value3').value;
     if (document.getElementById('slider3').disabled) {
         slider3Value = "NotEnabled";
     }
-    sendToMain("toMain", [slider1Value, slider2Value * 2, slider3Value]);
+
+    window.api.send("toMain", [slider1Value, slider2Value*2, slider3Value]);
 }
 
-function resetValues() {
-    const sliders = [1, 2, 3];
-    sliders.forEach(num => {
-        document.getElementById(`slider${num}`).value = 0;
-        document.getElementById(`value${num}`).value = 0;
-    });
+
+function ResetValues() {
+    document.getElementById('slider1').value = 0;
+    document.getElementById('value1').value = 0;
+    document.getElementById('slider2').value = 0;
+    document.getElementById('value2').value = 0;
+    document.getElementById('slider3').value = 0;
+    document.getElementById('value3').value = 0;
     document.getElementById("enablepowermanagement").checked = false;
     document.getElementById('slider3').disabled = true;
     document.getElementById('value3').disabled = true;
     getSliderValues();
 }
+
+
+function confirmCheckbox(event) {
+    const checkbox = event.target;
+    if (checkbox.checked) {
+        if (!confirm("Are you sure you want to enable power management ?\nThis is a dangerous feature and you should know what you're doing before enabling it.\nThis feature won't work on Laptop GPU.")) {
+            checkbox.checked = false;
+        } else {
+            document.getElementById('slider3').disabled = false;
+            document.getElementById('value3').disabled = false;
+        }
+    } else {
+        document.getElementById('slider3').disabled = true;
+        document.getElementById('value3').disabled = true;
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const checkbox = document.getElementById("enablepowermanagement");
+    checkbox.addEventListener("change", confirmCheckbox);
+
+    document.getElementById('slider3').disabled = true;
+    document.getElementById('value3').disabled = true;
+});
